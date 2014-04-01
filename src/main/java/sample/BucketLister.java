c6c0b898175b8d8d5e48ce382e652cc0b9b1b0fd
d-smith <doug.smith.mail@gmail.com>
2014-04-01 08:08:45 -0600

    Working bucket lister

---
+++ src/main/java/sample/BucketLister.java
@@ -0,0 +1,24 @@
+package sample;
+
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+
+public class BucketLister {
+    public static void main(String[] args) throws Exception {
+
+        ClientConfiguration config = new ClientConfiguration()
+                .withProxyHost("http.proxy.fmr.com")
+                .withProxyPort(8000);
+
+       AmazonS3 s3 = new AmazonS3Client(new ClasspathPropertiesFileCredentialsProvider(), config);
+
+        System.out.println("Listing buckets");
+        for (Bucket bucket : s3.listBuckets()) {
+            System.out.println(" - " + bucket.getName());
+        }
+    }
+}
