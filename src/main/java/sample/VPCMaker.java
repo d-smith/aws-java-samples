e1885e54fb1f04ec63534fbfc3c149286e2e7444
d-smith <doug.smith.mail@gmail.com>
2014-04-01 10:44:35 -0600

    Completed creation of VPC with public subnet

--- src/main/java/sample/VPCMaker.java
+++ src/main/java/sample/VPCMaker.java
@@ -4,11 +4,10 @@ package sample;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2Client;
-import com.amazonaws.services.ec2.model.CreateVpcRequest;
-import com.amazonaws.services.ec2.model.CreateVpcResult;
-import com.amazonaws.services.ec2.model.Vpc;
+import com.amazonaws.services.ec2.model.*;
 
 public class VPCMaker {
+
     public static Vpc createVPC(AmazonEC2Client client, String cidr) {
         CreateVpcResult vpcResult = client.createVpc(
                 new CreateVpcRequest(cidr)
@@ -16,6 +15,36 @@ public class VPCMaker {
 
         return vpcResult.getVpc();
     }
+
+    public static Subnet createSubnet(AmazonEC2Client client, String vpcId, String cidr, String az) {
+        CreateSubnetResult subnetResult = client.createSubnet(
+                new CreateSubnetRequest(vpcId, cidr)
+                        .withAvailabilityZone(az)
+        );
+
+        return subnetResult.getSubnet();
+    }
+
+    public static String createAndAttachInternetGatewayToVPC(AmazonEC2Client client, Vpc vpc) {
+        CreateInternetGatewayResult igwResult = client.createInternetGateway();
+        client.attachInternetGateway(
+                new AttachInternetGatewayRequest()
+                        .withVpcId(vpc.getVpcId())
+                        .withInternetGatewayId(igwResult.getInternetGateway().getInternetGatewayId())
+        );
+
+        return igwResult.getInternetGateway().getInternetGatewayId();
+    }
+
+
+    public static RouteTable createRouteTableForVPC(AmazonEC2Client client, Vpc vpc) {
+        CreateRouteTableResult rtr = client.createRouteTable(
+                new CreateRouteTableRequest().withVpcId(vpc.getVpcId())
+        );
+
+        return rtr.getRouteTable();
+    }
+
 
     public static void main(String[] args) throws Exception {
 
@@ -29,6 +58,31 @@ public class VPCMaker {
 
         Vpc vpc = createVPC(client, "10.0.0.0/16");
         System.out.println(vpc.toString());
+
+        Subnet subnet = createSubnet(client, vpc.getVpcId(), "10.0.0.0/24", "us-east-1d");
+        System.out.println(subnet);
+
+        String gatewayId = createAndAttachInternetGatewayToVPC(client, vpc);
+
+        RouteTable routeTable = createRouteTableForVPC(client, vpc);
+
+        System.out.println(routeTable);
+
+
+        client.createRoute(
+                new CreateRouteRequest()
+                    .withRouteTableId(routeTable.getRouteTableId())
+                    .withDestinationCidrBlock("0.0.0.0/0")
+                    .withGatewayId(gatewayId)
+        );
+
+        AssociateRouteTableResult associateRouteTableResult = client.associateRouteTable(
+            new AssociateRouteTableRequest()
+                .withRouteTableId(routeTable.getRouteTableId())
+                .withSubnetId(subnet.getSubnetId())
+        );
+
+        System.out.println("association id (route table to subnet): " + associateRouteTableResult.getAssociationId());
 
     }
 }
